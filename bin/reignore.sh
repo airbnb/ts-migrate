771bf3a1961b2a52472ae47f4a80655c1dc43e3a
Sergii Rudenko <rudenko.sergey92@gmail.com>
2020-01-22 13:37:38 -0800

    Better placement

--- bin/reignore.sh
+++ bin/reignore.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 NODE_OPTIONS=--max_old_space_size=8192 ./node_modules/.bin/jest \
-  --config frontend/ts-migrate/bin/jest-config-reignore.js \
+  --config frontend/ts-migrate/scripts/jest-config-reignore.js \
   --json \
   --outputFile=tmp/ts-migrate-reignore-output.json \
   "$@"
