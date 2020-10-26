48f5452d1145274ccb898c646b2bd4e42ae2aac2
Evan Shaw <evan@vendhq.com>
2020-10-27 08:48:51 +1300

    Update docs and examples

--- packages/ts-migrate-example/src/index.ts
+++ packages/ts-migrate-example/src/index.ts
@@ -1,5 +1,5 @@
 import path from 'path';
-import { forkTSServer, migrate, MigrateConfig } from 'ts-migrate-server';
+import { migrate, MigrateConfig } from 'ts-migrate-server';
 
 import examplePluginTs from './example-plugin-ts';
 import examplePluginText from './example-plugin-text';
@@ -8,20 +8,14 @@ import examplePluginJscodeshift from './example-plugin-jscodeshift';
 // it will change content of the index.ts in the input folder
 async function runMigration() {
   const inputDir = path.resolve(__dirname, 'input');
-  const server = forkTSServer();
-
-  process.on('exit', () => {
-    server.kill();
-  });
 
   const config = new MigrateConfig()
     .addPlugin(examplePluginJscodeshift, {})
     .addPlugin(examplePluginTs, { shouldReplaceText: true })
     .addPlugin(examplePluginText, {});
 
-  const exitCode = await migrate({ rootDir: inputDir, config, server });
+  const exitCode = await migrate({ rootDir: inputDir, config });
 
-  server.kill();
   process.exit(exitCode);
 }
 
