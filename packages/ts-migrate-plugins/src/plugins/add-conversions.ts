b7614a532628d48ed83411b4324a41436a283057
Evan Shaw <evan@vendhq.com>
2021-06-10 22:02:02 +1200

    Use UpdateTracker in add-conversions plugin

--- packages/ts-migrate-plugins/src/plugins/add-conversions.ts
+++ packages/ts-migrate-plugins/src/plugins/add-conversions.ts
@@ -3,6 +3,7 @@ import { Plugin } from 'ts-migrate-server';
 import { isDiagnosticWithLinePosition } from '../utils/type-guards';
 import getTokenAtPosition from './utils/token-pos';
 import { AnyAliasOptions, validateAnyAliasOptions } from '../utils/validateOptions';
+import UpdateTracker from './utils/update';
 
 type Options = AnyAliasOptions;
 
@@ -16,20 +17,16 @@ const supportedDiagnostics = new Set([
 const addConversionsPlugin: Plugin<Options> = {
   name: 'add-conversions',
 
-  run({ fileName, sourceFile, text, options, getLanguageService }) {
+  run({ fileName, sourceFile, options, getLanguageService }) {
     // Filter out diagnostics we care about.
     const diags = getLanguageService()
       .getSemanticDiagnostics(fileName)
       .filter(isDiagnosticWithLinePosition)
       .filter((diag) => supportedDiagnostics.has(diag.code));
 
-    const result = ts.transform(sourceFile, [addConversionsTransformerFactory(diags, options)]);
-    const newSourceFile = result.transformed[0];
-    if (newSourceFile === sourceFile) {
-      return text;
-    }
-    const printer = ts.createPrinter();
-    return printer.printFile(newSourceFile);
+    const updates = new UpdateTracker(sourceFile);
+    ts.transform(sourceFile, [addConversionsTransformerFactory(updates, diags, options)]);
+    return updates.apply();
   },
 
   validate: validateAnyAliasOptions,
@@ -38,6 +35,7 @@ const addConversionsPlugin: Plugin<Options> = {
 export default addConversionsPlugin;
 
 const addConversionsTransformerFactory = (
+  updates: UpdateTracker,
   diags: ts.DiagnosticWithLocation[],
   { anyAlias }: Options,
 ) => (context: ts.TransformationContext) => {
@@ -70,16 +68,113 @@ const addConversionsTransformerFactory = (
         })
         .filter((node): node is ts.Expression => node !== null),
     );
-    return ts.visitNode(file, visit);
+    visit(file);
+    return file;
   };
 
-  function visit(origNode: ts.Node): ts.Node {
+  function visit(origNode: ts.Node): ts.Node | undefined {
     const needsConversion = nodesToConvert.has(origNode);
-    const node = ts.visitEachChild(origNode, visit, context);
-    if (!needsConversion) {
-      return node;
+    let node = ts.visitEachChild(origNode, visit, context);
+    if (node === origNode && !needsConversion) {
+      return origNode;
     }
 
-    return factory.createAsExpression(node as ts.Expression, anyType);
+    if (needsConversion) {
+      node = factory.createAsExpression(node as ts.Expression, anyType);
+    }
+
+    if (shouldReplace(node)) {
+      replaceNode(origNode, node);
+      return origNode;
+    }
+
+    return node;
+  }
+
+  // Nodes that have one expression child called "expression".
+  type ExpressionChild =
+    | ts.DoStatement
+    | ts.IfStatement
+    | ts.SwitchStatement
+    | ts.WithStatement
+    | ts.WhileStatement;
+
+  /**
+   * For nodes that contain both expression and statement children, only
+   * replace the direct expression children. The statements have already
+   * been replaced at a lower level and replacing them again can produce
+   * duplicate statements or invalid syntax.
+   */
+  function replaceNode(origNode: ts.Node, newNode: ts.Node): void {
+    switch (origNode.kind) {
+      case ts.SyntaxKind.DoStatement:
+      case ts.SyntaxKind.IfStatement:
+      case ts.SyntaxKind.SwitchStatement:
+      case ts.SyntaxKind.WithStatement:
+      case ts.SyntaxKind.WhileStatement:
+        updates.replaceNode(
+          (origNode as ExpressionChild).expression,
+          (newNode as ExpressionChild).expression,
+        );
+        break;
+
+      case ts.SyntaxKind.ForStatement:
+        updates.replaceNode(
+          (origNode as ts.ForStatement).initializer,
+          (newNode as ts.ForStatement).initializer,
+        );
+        updates.replaceNode(
+          (origNode as ts.ForStatement).condition,
+          (newNode as ts.ForStatement).condition,
+        );
+        updates.replaceNode(
+          (origNode as ts.ForStatement).incrementor,
+          (newNode as ts.ForStatement).incrementor,
+        );
+        break;
+
+      case ts.SyntaxKind.ForInStatement:
+      case ts.SyntaxKind.ForOfStatement:
+        updates.replaceNode(
+          (origNode as ts.ForInOrOfStatement).expression,
+          (newNode as ts.ForInOrOfStatement).expression,
+        );
+        updates.replaceNode(
+          (origNode as ts.ForInOrOfStatement).initializer,
+          (newNode as ts.ForInOrOfStatement).initializer,
+        );
+        break;
+
+      default:
+        updates.replaceNode(origNode, newNode);
+        break;
+    }
   }
 };
+
+/**
+ * Determines whether a node is eligible to be replaced.
+ *
+ * Replacing only the expression may produce invalid syntax due to missing parentheses.
+ * There is still some risk of losing whitespace if the expression is contained within
+ * an if statement condition or other construct that can contain blocks.
+ */
+function shouldReplace(node: ts.Node): boolean {
+  if (isStatement(node)) {
+    return true;
+  }
+  switch (node.kind) {
+    case ts.SyntaxKind.CaseClause:
+    case ts.SyntaxKind.ClassDeclaration:
+    case ts.SyntaxKind.EnumMember:
+    case ts.SyntaxKind.HeritageClause:
+    case ts.SyntaxKind.SourceFile: // In case we missed any other case.
+      return true;
+    default:
+      return false;
+  }
+}
+
+function isStatement(node: ts.Node): node is ts.Statement {
+  return ts.SyntaxKind.FirstStatement <= node.kind && node.kind <= ts.SyntaxKind.LastStatement;
+}
